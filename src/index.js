92d95f4a09c32e99011d80c091637c3cb15083ef
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-05-24 19:49:47 +0300

    Enhance layout and styles: add new icons, improve responsiveness, and update component styles

--- src/index.js
+++ src/index.js
@@ -12,8 +12,12 @@ function Content() {
   return (
     <main className="content">
       <section>
+        <section className="section-header lg:flex hidden">
+          <Avatar src="public/gussie.jpg" size="lg" />
+          <Title size="lg">Dollie Blair</Title>
+        </section>
         <NavList className="video-list">
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover.jpg"
@@ -23,7 +27,7 @@ function Content() {
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover2.jpg"
@@ -33,7 +37,7 @@ function Content() {
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover3.jpg"
@@ -43,7 +47,7 @@ function Content() {
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover4.jpg"
@@ -53,7 +57,7 @@ function Content() {
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover5.jpg"
@@ -63,7 +67,7 @@ function Content() {
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover6.jpg"
@@ -77,37 +81,52 @@ function Content() {
       </section>
 
       <section className="home-recommended">
-        <Title size="lg" className="home-recommended__title">
-          Recommended
-        </Title>
+        <header className="section-header">
+          <Title size="lg" className="home-recommended__title">
+            Recommended
+          </Title>
+          <div className="content-extra ml-auto lg:flex items-center gap-40">
+            <div className="flex items-center gap-10">
+              <Button type="round">
+                <Icon name="chevron-left" size={11}></Icon>
+              </Button>
+              <Button type="round">
+                <Icon name="chevron-right" size={11}></Icon>
+              </Button>
+            </div>
+          </div>
+        </header>
         <NavList className="video-list">
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover7.jpg"
                 duration="2:30"
                 views="80k"
                 title="A Brief History of Creation"
+                size="lg"
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover8.jpg"
                 duration="8:00"
                 views="123k"
                 title="Selecting the Right Hotel"
+                size="lg"
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover9.jpg"
                 duration="6:40"
                 views="11k"
                 title="Asteroids"
+                size="lg"
               />
             </Link>
           </NavListItem>
@@ -116,14 +135,28 @@ function Content() {
 
       <section className="home-recommended">
         <header className="section-header">
-          <Avatar src="public/fooddrink.jpg" />
+          <Avatar src="public/food.jpg" size="lg" className="content-extra" />
           <Title size="lg" className="home-recommended__title">
             Food &amp; Drink
           </Title>
+          <small className="small content-extra">
+            Recommended channel for you
+          </small>
+          <div className="content-extra ml-auto lg:flex items-center gap-40">
+            <Button type="primary">Subscribe 2.3m</Button>
+            <div className="flex items-center gap-10">
+              <Button type="round">
+                <Icon name="chevron-left" size={11}></Icon>
+              </Button>
+              <Button type="round">
+                <Icon name="chevron-right" size={11}></Icon>
+              </Button>
+            </div>
+          </div>
         </header>
 
         <NavList className="video-list">
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover10.jpg"
@@ -133,7 +166,7 @@ function Content() {
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover11.jpg"
@@ -143,7 +176,7 @@ function Content() {
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover12.jpg"
@@ -153,7 +186,7 @@ function Content() {
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover13.jpg"
@@ -164,7 +197,7 @@ function Content() {
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover14.jpg"
@@ -175,7 +208,7 @@ function Content() {
               />
             </Link>
           </NavListItem>
-          <NavListItem>
+          <NavListItem className="video-list__item">
             <Link>
               <VideoCard
                 src="public/cover15.jpg"
@@ -198,6 +231,7 @@ function VideoCard(props) {
     title,
     src,
     alt,
+    size = "sm",
     views = "80k",
     createdAt = "3 days ago",
     author = "Dollie Blair",
@@ -207,7 +241,11 @@ function VideoCard(props) {
   return (
     <figure className={`video-card ${className ?? ""}`} {...rest}>
       <div className="relative">
-        <img src={src} alt={alt} className="video-card__cover" />
+        <img
+          src={src}
+          alt={alt}
+          className={`video-card__cover video-card__cover_${size}`}
+        />
         <time datetime="PT2M30S" className="video-card__duration">
           {duration}
         </time>
@@ -296,18 +334,28 @@ function Search(props) {
 }
 
 function Avatar(props) {
-  const { src = "public/dude.jpg", ...rest } = props;
+  const { className, src = "public/dude.jpg", size = "sm", ...rest } = props;
   return (
-    <svg viewBox="0 0 24 24" width="24" height="24" {...rest}>
-      <image href={src} width="24" height="24" clipPath="url(#clip-circle)" />
+    <svg
+      viewBox="0 0 24 24"
+      className={`avatar avatar_${size} ${className ?? ""}`}
+      {...rest}
+    >
+      <image
+        href={src}
+        width="24"
+        height="24"
+        clipPath="url(#clip-circle)"
+        preserveAspectRatio="xMidYMid slice"
+      />
     </svg>
   );
 }
 
 function Button(props) {
-  const { className, ...rest } = props;
+  const { className, type = "def", ...rest } = props;
   return (
-    <button className={`btn ${className ?? ""}`} {...rest}>
+    <button className={`btn ${className ?? ""} btn_${type}`} {...rest}>
       {props.children}
     </button>
   );
@@ -367,13 +415,112 @@ function Sidebar() {
             <span>Subscriptions</span>
           </Link>
         </NavListItem>
-        <NavListItem>
+        <NavListItem className="sidebar__navlist-spacer">
           <Link className="sidebar__navlist-link">
             <Icon name="folder" className="sidebar__navlist-icon" />
             <span>Library</span>
           </Link>
         </NavListItem>
+        <NavListItem className="sidebar-extra">
+          <Link className="sidebar__navlist-link">
+            <Icon name="scroll" className="sidebar__navlist-icon" />
+            <span>History</span>
+          </Link>
+        </NavListItem>
+        <NavListItem className="sidebar-extra">
+          <Link className="sidebar__navlist-link">
+            <Icon name="clock" className="sidebar__navlist-icon" />
+            <span>Watch Later</span>
+          </Link>
+        </NavListItem>
+        <NavListItem className="sidebar-extra">
+          <Link className="sidebar__navlist-link">
+            <Icon name="star" className="sidebar__navlist-icon" />
+            <span>Favorites</span>
+          </Link>
+        </NavListItem>
+        <NavListItem className="sidebar-extra">
+          <Link className="sidebar__navlist-link">
+            <Icon name="heart" className="sidebar__navlist-icon" />
+            <span>Liked Videos</span>
+          </Link>
+        </NavListItem>
+        <NavListItem className="sidebar-extra">
+          <Link className="sidebar__navlist-link">
+            <Icon name="musical-note" className="sidebar__navlist-icon" />
+            <span>Music</span>
+          </Link>
+        </NavListItem>
+        <NavListItem className="sidebar-extra">
+          <Link className="sidebar__navlist-link">
+            <Icon name="gamepad" className="sidebar__navlist-icon" />
+            <span>Games</span>
+          </Link>
+        </NavListItem>
+        <NavListItem className="sidebar-extra">
+          <Link className="sidebar__navlist-link">
+            <Icon name="chevron-down" className="sidebar__navlist-icon" />
+            <span>Show More</span>
+          </Link>
+        </NavListItem>
       </NavList>
+
+      <section className="sidebar-extra sidebar-section">
+        <Title className="mb-30">Subscriptions</Title>
+        <NavList className="sidebar__navlist gap-20">
+          <NavListItem>
+            <Link className="sidebar__navlist-link sidebar__navlist-link">
+              <Avatar
+                src="public/gussie.jpg"
+                className="sidebar__navlist-icon"
+              />
+              <span>Gussie Singleton</span>
+            </Link>
+          </NavListItem>
+          <NavListItem>
+            <Link className="sidebar__navlist-link sidebar__navlist-link">
+              <Avatar src="public/nora.jpg" className="sidebar__navlist-icon" />
+              <span>Nora Francis</span>
+            </Link>
+          </NavListItem>
+          <NavListItem>
+            <Link className="sidebar__navlist-link sidebar__navlist-link">
+              <Avatar
+                src="public/belle.jpg"
+                className="sidebar__navlist-icon"
+              />
+              <span>Belle Briggs</span>
+            </Link>
+          </NavListItem>
+          <NavListItem>
+            <Link className="sidebar__navlist-link sidebar__navlist-link">
+              <Avatar
+                src="public/eunice.jpg"
+                className="sidebar__navlist-icon"
+              />
+              <span>Eunice Cortez</span>
+            </Link>
+          </NavListItem>
+          <NavListItem>
+            <Link className="sidebar__navlist-link sidebar__navlist-link">
+              <Avatar src="public/emma.jpg" className="sidebar__navlist-icon" />
+              <span>Emma Hanson</span>
+            </Link>
+          </NavListItem>
+          <NavListItem>
+            <Link className="sidebar__navlist-link sidebar__navlist-link">
+              <Avatar src="public/leah.jpg" className="sidebar__navlist-icon" />
+              <span>Leah Berry</span>
+            </Link>
+          </NavListItem>
+        </NavList>
+      </section>
+      <div className="sidebar__settings">
+        <Button className="flex gap-10">
+          <Icon name="cog" />
+          <span className="ml-4">Settings</span>
+        </Button>
+      </div>
     </aside>
   );
 }
