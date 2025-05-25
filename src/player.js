b0c051b70a63e3db718a1e23c4cc68ccc6dd00b3
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-05-25 22:39:31 +0300

    feat: enhance player layout with new controls, icons, and styles; add switch component

--- src/player.js
+++ src/player.js
@@ -4,6 +4,80 @@ function App() {
       <Header />
       <Sidebar />
       <Content />
+
+      <aside className="upnext">
+        <section className="flex justify-between my-10">
+          <Title size="lg">Next</Title>
+          <div className="flex items-center gap-10">
+            <label htmlFor="autoplay" className="uppercase text-10 font-bold">
+              Autoplay
+            </label>
+            <Switch on id="autoplay" />
+          </div>
+        </section>
+        <NavList className="flex flex-col gap-20">
+          <NavListItem className="">
+            <Link>
+              <VideoCard
+                src="public/cover10.jpg"
+                duration="2:30"
+                views="80k"
+                title="A Brief History of Creation"
+              />
+            </Link>
+          </NavListItem>
+          <NavListItem>
+            <Link>
+              <VideoCard
+                src="public/cover11.jpg"
+                duration="8:00"
+                views="123k"
+                title="Selecting the Right Hotel"
+              />
+            </Link>
+          </NavListItem>
+          <NavListItem>
+            <Link>
+              <VideoCard
+                src="public/cover19.jpg"
+                duration="6:40"
+                views="11k"
+                title="Asteroids"
+              />
+            </Link>
+          </NavListItem>
+          <NavListItem>
+            <Link>
+              <VideoCard
+                src="public/cover4.jpg"
+                duration="6:40"
+                views="11k"
+                title="Telescopes 101"
+              />
+            </Link>
+          </NavListItem>
+          <NavListItem>
+            <Link>
+              <VideoCard
+                src="public/cover5.jpg"
+                duration="6:40"
+                views="11k"
+                title="Telescopes 101"
+              />
+            </Link>
+          </NavListItem>
+          <NavListItem>
+            <Link>
+              <VideoCard
+                src="public/cover6.jpg"
+                duration="6:40"
+                views="11k"
+                title="Telescopes 101"
+              />
+            </Link>
+          </NavListItem>
+        </NavList>
+      </aside>
     </>
   );
 }
@@ -14,7 +88,7 @@ function Header() {
       <Button className="header__menu">
         <Icon name="menu" />
       </Button>
-      <Logo />
+      <Logo className="logo_player" />
       <Search className="header__search lg:ml-50" />
       <Button className="header__btn-search">
         <Icon name="search" />
@@ -32,7 +106,10 @@ function Header() {
           </Icon>
         </Button>
       </div>
-      <Button>
+      <Button className="ml-auto lg:hidden">
+        <Icon name="dots" />
+      </Button>
+      <Button className="hidden lg:block">
         <Avatar src="public/dude.jpg" />
       </Button>
     </header>
@@ -41,218 +118,162 @@ function Header() {
 
 function Content() {
   return (
-    <main className="content">
-      <section className="home-section">
-        <section className="section-header lg:flex hidden">
-          <Avatar src="public/gussie.jpg" size="lg" />
-          <Title size="lg">Dollie Blair</Title>
-        </section>
-        <NavList className="video-list">
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover.jpg"
-                duration="2:30"
-                views="80k"
-                title="A Brief History of Creation"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover2.jpg"
-                duration="8:00"
-                views="123k"
-                title="Selecting the Right Hotel"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover3.jpg"
-                duration="6:40"
-                views="11k"
-                title="Asteroids"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover4.jpg"
-                duration="6:40"
-                views="11k"
-                title="Telescopes 101"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover5.jpg"
-                duration="6:40"
-                views="11k"
-                title="Telescopes 101"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover6.jpg"
-                duration="6:40"
-                views="11k"
-                title="Telescopes 101"
-              />
-            </Link>
-          </NavListItem>
-        </NavList>
-      </section>
+    <main className="content content_player">
+      <figure className="relative">
+        <svg viewBox="0 0 320 180" width="100%" className="lg:rounded">
+          <image
+            href="public/cover18.jpg"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="xMidYMid slice"
+          />
+        </svg>
 
-      <section className="home-section">
-        <header className="section-header">
-          <Title size="lg" className="home-section__title">
-            Recommended
-          </Title>
-          <div className="content-extra ml-auto lg:flex items-center gap-40">
-            <div className="flex items-center gap-10">
-              <Button type="round">
-                <Icon name="chevron-left" size={11}></Icon>
+        <div className="player-controls lg:hidden">
+          <Button className="shrink-0">
+            <Icon name="pause" size={12} />
+          </Button>
+          <time className="text-10">1:34</time>
+          <div className="player-progress w-full">
+            <div className="player-progress__track"></div>
+          </div>
+          <time className="text-10">-10:00</time>
+          <Button className="shrink-0">
+            <Icon name="volume" size={12} />
+          </Button>
+        </div>
+
+        <div className="player-controls-hd">
+          <div className="flex justify-between">
+            <time className="text-16">1:34</time>
+            <time className="text-16">-10:00</time>
+          </div>
+          <div className="player-progress w-full">
+            <div className="player-progress__track"></div>
+          </div>
+          <div className="flex justify-between">
+            <div className="flex gap-40">
+              <Button className="shrink-0">
+                <Icon name="pause" size={20} />
               </Button>
-              <Button type="round">
-                <Icon name="chevron-right" size={11}></Icon>
+              <Button className="shrink-0">
+                <Icon name="rewind" size={20} />
+              </Button>
+              <div className="flex items-center gap-10">
+                <Button className="shrink-0">
+                  <Icon name="volume" size={20} />
+                </Button>
+                <div className="player-volume">
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            <div className="color-em flex gap-20">
+              <Button className="shrink-0">
+                <Icon name="cc" size={20} />
+              </Button>
+              <Button className="shrink-0">
+                <Icon name="cog" size={20} />
+              </Button>
+              <Button className="shrink-0">
+                <Icon name="theater" size={20} />
+              </Button>
+              <Button className="shrink-0">
+                <Icon name="fullscreen" size={20} />
               </Button>
             </div>
           </div>
-        </header>
-        <NavList className="video-list">
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover7.jpg"
-                duration="2:30"
-                views="80k"
-                title="A Brief History of Creation"
-                size="lg"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover8.jpg"
-                duration="8:00"
-                views="123k"
-                title="Selecting the Right Hotel"
-                size="lg"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover9.jpg"
-                duration="6:40"
-                views="11k"
-                title="Asteroids"
-                size="lg"
-              />
-            </Link>
-          </NavListItem>
-        </NavList>
-      </section>
+        </div>
+      </figure>
 
-      <section className="home-section">
-        <header className="section-header">
-          <Avatar src="public/food.jpg" size="lg" className="content-extra" />
-          <Title size="lg" className="home-section__title">
-            Food &amp; Drink
-          </Title>
-          <small className="small content-extra">
-            Recommended channel for you
-          </small>
-          <div className="content-extra ml-auto lg:flex items-center gap-40">
-            <Button type="primary">Subscribe 2.3m</Button>
-            <div className="flex items-center gap-10">
-              <Button type="round">
-                <Icon name="chevron-left" size={11}></Icon>
-              </Button>
-              <Button type="round">
-                <Icon name="chevron-right" size={11}></Icon>
-              </Button>
-            </div>
+      <header className="flex flex-col gap-10 my-10 lg:my-20 lg:flex-row lg:items-end lg:justify-between w-full">
+        <div className="flex flex-col px-16 lg:p-0">
+          <div className="flex justify-between items-center">
+            <Title className="lg:text-36">
+              Dude You'Re Getting A Telescope
+            </Title>
+            <Button className="lg:hidden">
+              <Icon name="chevron-down" />
+            </Button>
           </div>
-        </header>
+          <span className="color-em">123k views</span>
+        </div>
+        <div className="flex gap-30 overflow-auto pl-16 py-10 lg:p-0 lg:gap-10">
+          <Button type="secondary">
+            <Icon name="like" className="color-em" />
+            <span>123k</span>
+          </Button>
+          <Button type="secondary" className="shrink-0">
+            <Icon name="dislike" className="color-em" />
+            <span>23k</span>
+          </Button>
+          <Button type="secondary" className="shrink-0">
+            <Icon name="share" className="color-em" />
+            <span>Share</span>
+          </Button>
+        </div>
+      </header>
 
-        <NavList className="video-list">
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover10.jpg"
-                duration="2:30"
-                views="80k"
-                title="Astronomy or Astrology"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover11.jpg"
-                duration="8:00"
-                views="123k"
-                title="Advertising Outdoors"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover12.jpg"
-                duration="6:40"
-                views="11k"
-                title="Radio Astronomy"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover13.jpg"
-                duration="6:40"
-                views="45k"
-                createdAt="2 months ago"
-                title="A Good Autoresponder"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover14.jpg"
-                duration="1:56"
-                views="86k"
-                createdAt="7 days ago"
-                title="Baby Monitoring Technology"
-              />
-            </Link>
-          </NavListItem>
-          <NavListItem className="video-list__item">
-            <Link>
-              <VideoCard
-                src="public/cover15.jpg"
-                duration="8:56"
-                views="8k"
-                createdAt="3 days ago"
-                title="Asteroids"
-              />
-            </Link>
-          </NavListItem>
-        </NavList>
+      <section className="subscribe flex justify-between items-center w-full">
+        <div className="flex gap-10 items-center w-full lg:items-start">
+          <Avatar src="public/food.jpg" size="lg" className="shrink-0" />
+          <div className="flex flex-col gap-10 max-w-prose">
+            <div className="flex flex-col gap-5 lg:gap-10">
+              <Title>Food & Drink</Title>
+              <span className="color-em text-10 lg:hidden">
+                245k subscribed
+              </span>
+              <span className="color-em text-10 hidden lg:block">
+                Published on 14 Jun 2019
+              </span>
+            </div>
+            <Text className="hidden lg:block">
+              A successful marketing plan relies heavily on the pulling-power of
+              advertising copy. Writing result-oriented ad copy is difficult, as
+              it must appeal to, entice, and convince consumers to take action.
+              There is no magic formula to write perfect ad copy; it is based on
+              a number of factors, including ad placement, demographic, even the
+              consumer’s mood when they see your ad.
+            </Text>
+            <Button className="hidden lg:flex text-20 color-em">
+              Show More
+            </Button>
+          </div>
+          <Button className="color-primary ml-auto lg:hidden">Subscribe</Button>
+          <Button className="hidden lg:flex ml-auto" type="primary">
+            Subscribe 2.3m
+          </Button>
+        </div>
       </section>
     </main>
+  );
+}
+
+function Switch(props) {
+  const { on, id, ...rest } = props;
+  return (
+    <span className="switch">
+      <input
+        type="checkbox"
+        id={id}
+        defaultChecked={on ? true : false}
+        className="switch__input"
+      />
+      <span className="switch__thumb"></span>
+      <span className="switch__track"></span>
+    </span>
+  );
+}
+
+function Text(props) {
+  const { children, size, ...rest } = props;
+  return (
+    <p
+      className={`text ${props.className ?? ""} ${size ? `text_${size}` : ""}`}
+      {...rest}
+    >
+      {children}
+    </p>
   );
 }
 
@@ -385,9 +406,9 @@ function Icon(props) {
   );
 }
 
-function Logo() {
+function Logo(props) {
   return (
-    <a href="/">
+    <a href="/" {...props}>
       <img src="public/yt-logo.svg" alt="YT logo" />
     </a>
   );
@@ -395,7 +416,7 @@ function Logo() {
 
 function Sidebar() {
   return (
-    <aside className="sidebar">
+    <aside className="sidebar lg:hidden">
       <NavList className="sidebar__navlist">
         <NavListItem>
           <Link className="sidebar__navlist-link sidebar__navlist-link_active">
